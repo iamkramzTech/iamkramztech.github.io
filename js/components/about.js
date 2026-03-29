137f0cebecc1ffb5d5e657382bc7c37baec5a2c2
Mark Joseph Bernaldez <83441254+iamkramzTech@users.noreply.github.com>
2026-03-29 17:06:33 +0000

    Update about.js
    no refferer noopener

--- js/components/about.js
+++ js/components/about.js
@@ -6,7 +6,7 @@ export function About()
     <div class="text-center mb-4">
       <h2>About Me</h2>
       <p class="lead">Passionate and detail-oriented developer focused on building efficient, secure, and scalable systems that can seamlessly support evolving business needs and long-term growth. Previously worked as an SEO Blog Specialist on a Digital Marketing team, which shaped my understanding of business and user impact. Continuously learning and adapting.</p>
-      <a href="https://drive.google.com/file/d/18CIb5hFVyzGrmjM5u96gS-ueG5I1bLFI" target="_blank" class="btn btn-primary mt-2"><i class="bi bi-eye-fill"></i> View CV</a>
+      <a href="https://drive.google.com/file/d/18CIb5hFVyzGrmjM5u96gS-ueG5I1bLFI" target="_blank" rel="noopener noreferrer" class="btn btn-primary mt-2"><i class="bi bi-eye-fill"></i> View CV</a>
     </div>
    </div>
 </section>
